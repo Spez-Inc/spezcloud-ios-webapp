fe38af4dfc7846b4d49bc9a889a2f8a71bc808ad
Sarp Ertoksoz <38987850+sarpertoksoz@users.noreply.github.com>
2018-12-07 19:03:24 +0300

    Initial release

---
+++ js/redirect.js
@@ -0,0 +1,15 @@
+var standdiv = document.getElementById("standlone");
+var nostanddiv = document.getElementById("nostandlone");
+standdiv.style.display = "none";
+nostanddiv.style.display = "none";
+
+if (
+    ("standalone" in window.navigator) &&
+    !window.navigator.standalone
+    ){
+
+    standdiv.style.display = "block";
+	window.navigator.replace("https://kmu.files.cnow.at/login?redirect_url=/apps/files/");
+} else {
+	nostanddiv.style.display = "block";
+}
